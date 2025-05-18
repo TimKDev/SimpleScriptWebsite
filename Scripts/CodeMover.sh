1476a4508b6a425808b334ee9823459190d3954a
Tim Kempkens <tim.kempkens@googlemail.com>
2025-05-18 09:16:30 +0200

    Added CodeMover Script

---
+++ Scripts/CodeMover.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Source/projects/SimpleScriptWebSite/ConsoleApp/*
+
+cp -r  ~/Source/projects/SimpleScriptCompiler/SimpleScript.Compiler/bin/Debug/net9.0/*  ~/Source/projects/SimpleScriptWebSite/ConsoleApp
+
